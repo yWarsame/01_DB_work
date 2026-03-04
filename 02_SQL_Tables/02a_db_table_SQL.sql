0223522bf493e1169913bb9b54b1b270a12cccc1
yWarsame <yasinhassan1013@gmail.com>
2026-03-04 12:52:47 +0100

    02a/update

--- 02_SQL_Tables/02a_db_table_SQL.sql
+++ 02_SQL_Tables/02a_db_table_SQL.sql
@@ -6,25 +6,32 @@
 
 /* Datenbanken auf Server anzeigen */
 
-
+SHOW DATABASES;
 /* DB boo löschen, falls vorhanden*/
 
-
+DROP DATABASE IF EXISTS boo;
 /* DB boo anlegen, falls noch nicht vorhanden*/
-
+CREATE DATABASE IF NOT EXISTS boo;
 
 /* DB auswählen */
-
+USE boo;
 /* Tabelle anlegen */
-
+CREATE TABLE if NOT EXISTS test(
+    first_name VARCHAR(20),
+    age INT
+);
 
 /* Alle Tabellen in der DB anzeigen */
 
-
+SHOW TABLES;
 /* Struktur der Tabelle anzeigen */
 
-
+DESCRIBE test;
 /* ----- Daten ------- */
+INSERT INTO test(name,age) VALUES("Max",35);
+INSERT INTO test(age,name) VALUES(29,"Maxine");
+INSERT INTO test VALUES();
 
 
 /* ---- Inhalte der Tabelle anzeigen ---- */
+SELECT * FROM test;
