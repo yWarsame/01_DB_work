2c4fea8ae93800876032c1c8d01691ec90fcea71
yWarsame <yasinhassan1013@gmail.com>
2026-03-05 10:32:35 +0100

    02b / 02 Iteratuar

--- 02_SQL_Tables/02b_db_table_SQL.sql
+++ 02_SQL_Tables/02b_db_table_SQL.sql
@@ -1,4 +1,3 @@
-\! cls
 /* ------  Strukturen ----- */
 
 /* 	 
@@ -20,9 +19,10 @@ DROP TABLE IF EXISTS test;
 
 /* Tab. test anlegen, falls noch nicht vorhanden*/
 CREATE TABLE IF NOT EXISTS test
-(
-    name VARCHAR(20),
-    age INT
+( 
+     -- Constraints (Einschränkungen):
+    name VARCHAR(20) NOT NULL DEFAULT "TBA",
+    age INT NOT NULL DEFAULT 0
 );
 
 
@@ -35,10 +35,11 @@ DESCRIBE test;
 /* ----- Daten ------- */
 INSERT INTO test(name,age) VALUES("Max",35);
 INSERT INTO test(age,name) VALUES(29,"Maxine");
-INSERT INTO test VALUES();
+INSERT INTO test VALUES(DEFAULT, DEFAULT);  -- ← KORRIGIERT: Mit DEFAULT-Werten
+INSERT INTO test(name,age) VALUES("Max",35);
 
 -- ABER: Doppelte Datensätze werden zugelassen !
-
+INSERT INTO test(name,age) VALUES("Max",35);
 
 /* ---- Inhalte der Tabelle anzeigen ---- */
 SELECT * FROM test;
